6de9ef78fe315a6b95b8b6276728fae0866455d6
dear <toby_du@126.com>
2015-08-03 12:38:11 +0800

    receive parameter port from command line

--- Analysis.py
+++ Analysis.py
@@ -9,6 +9,7 @@ import urllib
 import urllib2
 import urlparse
 import random
+import sys
 
 from proxy import PROXIES
 from agents import AGENTS
@@ -30,33 +31,56 @@ class Analysis(restful.Resource):
 # 请求前需转义 &  为 %26    
 #curl -i http://127.0.0.1:5000/a -d "key=http://mp.weixin.qq.com/s?__biz=MjM5NzMzODM5Ng==&mid=207718447&idx=1&sn=5c22a71643b09c65d6c05176895c1814&3rd=MzA3MDU4NTYzMw==&scene=6#rd" -X PUT
    def put(self,url):
-        print 'bbb'
-        key = request.form['key']
+        try:
+            key = request.form['key']
+            print 'request from url [' + key + ']'
+            proxy = eval(json.dumps(random.choice(PROXIES)))['ip_port']
+            
+            req = urllib2.Request(key)
+            req.add_header('User-Agent',random.choice(AGENTS))
+            #req.meta['proxy'] = proxy
+            req.proxy=proxy
+            response = urllib2.urlopen(req)
+            d=pq(response.read())
+            item = {}
+            item['title'] = d('title').text()
+            item['datetime'] = d('#post-date').text()
+            item['author']=d('#post-user').text()
+            item['contenturl']=key
+            #item['tinyurl']='tinyurl'
+            #print d('.rich_media_content>p').text()
+            if d('.rich_media_content>section').text() == '':
+                print 'has no element section'
+                item['digest']=d('.rich_media_content>p').find('span').text()
+            else:
+                print 'has element section'
+                item['digest']=d('.rich_media_content>section').find('span').text()
+            return item 
+        except Exception,ex:
+            print 'error occurred while read info from url [' + key + ']'
+            print ex
+            return "{'msg':'error msg'}"
 
-        proxy = eval(json.dumps(random.choice(PROXIES)))['ip_port']
-        
-        req = urllib2.Request(key)
-        req.add_header('User-Agent',random.choice(AGENTS))
-        #req.meta['proxy'] = proxy
-        req.proxy=proxy
-        response = urllib2.urlopen(req)
-        d=pq(response.read())
-        item = {}
-        item['title'] = d('title').text()
-        item['datetime'] = d('#post-date').text()
-        item['author']=d('#post-user').text()
-        item['contenturl']=key
-        #item['tinyurl']='tinyurl'
-        item['digest']=d('.rich_media_content>p').find('span').text()
-        md = MongoDBPipeline()
-        md.save(item)
-        return item  
+        #md = MongoDBPipeline()
+        #md.save(item)
+         
          
 #定义restful接口及参数
 api.add_resource(Analysis, '/<string:url>')
+port = 5000
+if len(sys.argv)>=2:
+    port = sys.argv[1]
+
+try:
+    int(port)
+except Exception,ex:
+    print ex
+    print 'port number is invalid,use default port '
+    port = 5000
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=int(port))
+    #app.run(debug=True)
     
  
 
